6fa9266e37a433591f812fedf4891c18bc780273
Nahida Parveen <nahida.parveen00007@gmail.com>
2020-10-06 22:02:01 +0530

    Commit DemoMethod

--- Selenium-Java/src/Java_Programs/DemoMethod.java
+++ Selenium-Java/src/Java_Programs/DemoMethod.java
@@ -7,14 +7,15 @@ public class DemoMethod {
 		return num1+num2;
 	}
 	
-	public int myMethod(int var1, int var2) {
+	/*public int myMethod(int var1, int var2) {
 		System.out.println("Second method of DemoMethod");
 		return var1-var2;
-	}
+	}*/
 	public static void main(String[] args) {
 		DemoMethod dm = new DemoMethod();
-		dm.myMethod(20, 30);
-		dm.myMethod(10,5);
+		int s = dm.myMethod(20, 30);
+		// dm.myMethod(10,5);
+		System.out.println(s);
 
 	}
 
