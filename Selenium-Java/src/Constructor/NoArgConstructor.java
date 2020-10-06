00c90a459b1342b5f241a7cc1b9b7663f230322f
Nahida Parveen <nahida.parveen00007@gmail.com>
2020-10-06 23:40:00 +0530

    Commit Constructor - NO Argument

---
+++ Selenium-Java/src/Constructor/NoArgConstructor.java
@@ -0,0 +1,15 @@
+package Constructor;
+
+public class NoArgConstructor {
+	
+	NoArgConstructor() {
+		System.out.println("There is a no argument constructor ");
+	}
+
+	public static void main(String[] args) {
+		
+		new NoArgConstructor();
+
+	}
+
+}
