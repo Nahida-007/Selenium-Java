390ca7a3ab4158aa030482565f89ee52837c22fc
Nahida Parveen <nahida.parveen00007@gmail.com>
2020-10-26 21:23:59 +0530

    Commit Honda

---
+++ Selenium-Java/src/Abstraction/Honda.java
@@ -0,0 +1,19 @@
+package Abstraction;
+
+public class Honda extends Bike{
+	
+	@Override
+	void run() {
+		
+		System.out.println("running safely");
+	}
+
+
+	public static void main(String[] args) {
+		Bike obj = new Honda();
+		obj.run();
+
+	}
+
+	
+}
