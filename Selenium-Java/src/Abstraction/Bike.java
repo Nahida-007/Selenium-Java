6d5e0ef585a0777047e762f52631cabb1d154bfb
Nahida Parveen <nahida.parveen00007@gmail.com>
2020-10-26 22:24:35 +0530

    Commit Bike

--- Selenium-Java/src/Abstraction/Bike.java
+++ Selenium-Java/src/Abstraction/Bike.java
@@ -2,6 +2,10 @@ package Abstraction;
 
 public abstract class Bike {
 	
+	Bike(){
+		System.out.println("Bike is Created");
+	}
+	
 	abstract void run();
     void changeGear() {
     	System.out.println("Gear Changed");
